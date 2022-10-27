190de321d901c0ca96bd92b78cbba353c5175b30
jiseon.kim <jiseon.kim@bespinglobal.com>
2022-10-27 15:55:13 +0900

    Add dir1, dir2

---
+++ dir2/main.tf
@@ -0,0 +1,54 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.instance_name
+  machine_type = var.instance_type
+
+  labels = var.additional_labels
+
+  boot_disk {
+    initialize_params {
+      image = var.instance_image
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.pub-1.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "pub-1" {
+  name = "${var.subnet_prefix}-pub-1"
+  ip_cidr_range = "192.168.1.0/24"
+  network = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_subnetwork" "pub-2" {
+  name = "${var.subnet_prefix}-pub-2"
+  ip_cidr_range = "192.168.2.0/24"
+  network = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_subnetwork" "prv-1" {
+  name = "${var.subnet_prefix}-prv-1"
+  ip_cidr_range = "192.168.3.0/24"
+  network = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_subnetwork" "prv-2" {
+  name = "${var.subnet_prefix}-prv-2"
+  ip_cidr_range = "192.168.4.0/24"
+  network = google_compute_network.vpc_network.id
+}
---
+++ dir2/terraform.tfvars
@@ -0,0 +1,13 @@
+project_id = "opsnow-qa-automation"
+region = "asia-northeast3"
+zone = "asia-northeast3-a"
+instance_name = "catalog-terraform-provisioning-hi-instance"
+instance_type = "e2-micro"
+instance_image = "centos-7-v20200403"
+vpc_name = "catalog-terraform-provisioning-hi-network"
+subnet_prefix = "hiqa"
+additional_labels = {
+  "swqa" = "devops",
+  "created_by" = "hyein"
+  "directory"  = "dir2"
+}
